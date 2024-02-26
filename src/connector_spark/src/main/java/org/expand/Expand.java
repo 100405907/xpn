e766b8c4df903ff7848b1169f802acc2d534444a
100405907 <100405907@alumnos.uc3m.es>
2024-02-26 20:57:37 +0000

    sin destroy en xpn

--- src/connector_spark/src/main/java/org/expand/Expand.java
+++ src/connector_spark/src/main/java/org/expand/Expand.java
@@ -62,7 +62,6 @@ public class Expand extends FileSystem {
 
 	public void close() throws IOException {
 		System.out.println("------------------ENTRO A CLOSE------------------");
-		this.xpn.jni_xpn_destroy();
 		super.close();
 		System.out.println("------------------SALGO DE CLOSE------------------");
 	}
