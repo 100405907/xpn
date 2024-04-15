c5f4528b13cc491a2e764b1c6e35963e3d8b9aec
100405907 <100405907@alumnos.uc3m.es>
2024-04-15 08:43:29 +0000

    nuevo wc-hadoop

--- src/connector_spark/src/main/java/org/expand/tests/WordCount.java
+++ src/connector_spark/src/main/java/org/expand/tests/WordCount.java
@@ -1,11 +1,11 @@
 package org.expand.tests;
 
 import java.io.IOException;
-import java.util.StringTokenizer;
-
+import java.net.URI;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -14,67 +14,51 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 import org.expand.hadoop.Expand;
-import java.net.URI;
 
 public class WordCount {
-
-  public static class MapForWordCount 
-       extends Mapper<Object, Text, Text, IntWritable>{
-    
-    private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
-      
-    public void map(Object key, Text value, Context context
-                    ) throws IOException, InterruptedException {
-      StringTokenizer itr = new StringTokenizer(value.toString());
-      while (itr.hasMoreTokens()) {
-        word.set(itr.nextToken());
-        context.write(word, one);
-      }
+  public static void main(String [] args) throws Exception {
+    Configuration conf=new Configuration();
+    // conf.set("fs.defaultFS", "xpn:///");
+    // conf.set("fs.xpn.impl", "org.expand.hadoop.Expand");
+    // conf.set("mapreduce.job.split.metainfo.maxsize", "-1");
+    // conf.set("mapred.child.java.opts", "-Xmx4096m");
+    // Expand xpn = new Expand();
+    // xpn.initialize(URI.create("xpn:///"), conf);
+    Path input=new Path("hdfs://localhost:9000/2000-0.txt");
+    Path output=new Path("hdfs://localhost:9000/wc-out");
+    // xpn.delete(output, true);
+    long startTime = System.nanoTime();
+    Job j=new Job(conf,"wordcount");
+    j.setJarByClass(WordCount.class);
+    j.setMapperClass(MapForWordCount.class);
+    j.setReducerClass(ReduceForWordCount.class);
+    j.setOutputKeyClass(Text.class);
+    j.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(j, input);
+    FileOutputFormat.setOutputPath(j, output);
+    j.waitForCompletion(true);
+    System.out.println("---------------------------------- " + (System.nanoTime() - startTime) + " ---------------------------------");
+    System.exit(0);
+  }
+public static class MapForWordCount extends Mapper<LongWritable, Text, Text, IntWritable>{ 
+  public void map(LongWritable key, Text value, Context con) throws IOException, InterruptedException {
+    String line = value.toString();
+    String[] words=line.split(" ");
+    for(String word: words ) { 
+      Text outputKey = new Text(word.toUpperCase().trim());
+      IntWritable outputValue = new IntWritable(1);
+      con.write(outputKey, outputValue);
     }
   }
-  
-  public static class ReduceForWordCount 
-       extends Reducer<Text,IntWritable,Text,IntWritable> {
-    private IntWritable result = new IntWritable();
-
-    public void reduce(Text key, Iterable<IntWritable> values, 
-                       Context context
-                       ) throws IOException, InterruptedException {
-      int sum = 0;
-      for (IntWritable val : values) {
-        sum += val.get();
-      }
-      result.set(sum);
-      context.write(key, result);
+}
+public static class ReduceForWordCount extends Reducer<Text, IntWritable, Text, IntWritable> {
+  public void reduce(Text word, Iterable<IntWritable> values, Context con) throws IOException, InterruptedException {
+    int sum = 0;
+    for(IntWritable value : values)
+    {
+      sum += value.get();
     }
+    con.write(word, new IntWritable(sum));
   }
-
-  public static void main(String[] args) throws Exception {
-    Configuration conf = new Configuration();
-    conf.set("fs.defaultFS", "xpn:///");
-    conf.set("fs.xpn.impl", "org.expand.hadoop.Expand");
-    conf.set("mapreduce.job.split.metainfo.maxsize", "-1");
-    conf.set("mapred.child.java.opts", "-Xmx4096m");
-    Expand xpn = new Expand();
-    xpn.initialize(URI.create("xpn:///"), conf);
-    String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-    if (otherArgs.length < 2) {
-      System.err.println("Usage: wordcount <in> [<in>...] <out>");
-      System.exit(2);
-    }
-    Job job = Job.getInstance(conf, "word count");
-    job.setJarByClass(WordCount.class);
-    job.setMapperClass(MapForWordCount.class);
-    job.setCombinerClass(ReduceForWordCount.class);
-    job.setReducerClass(ReduceForWordCount.class);
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
-    for (int i = 0; i < otherArgs.length - 1; ++i) {
-      FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
-    }
-    FileOutputFormat.setOutputPath(job,
-      new Path(otherArgs[otherArgs.length - 1]));
-    System.exit(job.waitForCompletion(true) ? 0 : 1);
-  }
+ }
 }
