ec3104b7e9e94c8512400e6c89aa46ef476a03a8
100405907 <100405907@alumnos.uc3m.es>
2024-04-17 08:39:22 +0000

    prueba load quixote

--- src/connector_spark/src/main/java/org/expand/tests/loadQuixote.java
+++ src/connector_spark/src/main/java/org/expand/tests/loadQuixote.java
@@ -42,7 +42,7 @@ public class loadQuixote {
 
 		Expand xpn = new Expand();
 		String filePath = "xpn:///xpn/wikipedia";
-		String input = "file:///beegfs/home/javier.garciablas/gsotodos/data/wikipedia";
+		String input = "file:///home/lab/data/2000-0.txt";
 
 		try{
 			xpn.initialize(URI.create("xpn:///"), xpnconf);
