64786698988f762f02fa08c34051e9928b07c452
100405907 <100405907@alumnos.uc3m.es>
2024-05-13 16:55:51 +0000

    vuelta al path normal

--- src/connector_spark/benchmarks/spark-terasort/src/main/scala/com/github/ehiggs/spark/terasort/TeraInputFormat.scala
+++ src/connector_spark/benchmarks/spark-terasort/src/main/scala/com/github/ehiggs/spark/terasort/TeraInputFormat.scala
@@ -75,7 +75,7 @@ class TeraInputFormat extends FileInputFormat[Array[Byte], Array[Byte]] {
     }
 
     // val dirs: Array[Path] = FileInputFormat.getInputPaths(job)
-    val listing: Array[FileStatus] = xpn.listStatus(new Path("xpn:///"))
+    val listing: Array[FileStatus] = xpn.listStatus(new Path("xpn:///xpn/terasort_100"))
 
     // for (p <- dirs) {
     //   println(p.toString())
